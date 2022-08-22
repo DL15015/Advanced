01ed33e94f41b44750bae0947db7414719c40c0d
DL15015 <1478243841@qq.com>
2022-08-22 18:16:38 +0800

    master

--- TypeScript/TypeScript1.ts
+++ TypeScript/TypeScript1.ts
@@ -1,7 +1,9 @@
-function f(bina:number,){
-    var x=10;
-    return x+bina
-}
-n=f(100)
-console.log(n)
+// function f(bina: number) {
+//   var x = 10;
+//   return x + bina;
+// }
+// var n = f(100);
+// console.log(n);
 
+const net: number = 12;
+console.log(net);
---
+++ TypeScript/ts1.ts
@@ -0,0 +1,6 @@
+function cal(calu: number) {
+  var da = 99;
+  return da+calu;
+}
+const a = cal(1);
+console.log(a);
