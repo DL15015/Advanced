1d12d48437ff7354482b9fd685e916422d06a6a3
DL15015 <1478243841@qq.com>
2022-08-22 17:33:29 +0800

    update

---
+++ TypeScript/TypeScript1.ts
@@ -0,0 +1,7 @@
+function f(bina:number,){
+    var x=10;
+    return x+bina
+}
+n=f(100)
+console.log(n)
+
---
+++ TypeScript/TypeScript2.ts
@@ -0,0 +1,13 @@
+// var hello = "Hello World!";
+// console.log(hello);
+
+// console.log("Runoob");
+// console.log("hello");
+
+class Site {
+  name() {
+    console.log("我是中国人");
+  }
+}
+var obj = new Site();
+obj.name();
